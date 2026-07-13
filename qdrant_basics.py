1009b06579eefe700c1bf8630d83b8c66a146152
Kaushik Acharya <kaushik@palebluedot.net>
2026-07-13 15:51:50 -0700

    Initial commit

---
+++ qdrant_basics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from dotenv import load_dotenv
+from qdrant_client import QdrantClient, models
+import os
+
+load_dotenv()
+
+api_key = os.getenv("QDRANT_API_KEY")
+client = QdrantClient(url="https://2ec3c57d-71b6-449e-9d63-6c9404bfade3.us-west-1-0.aws.cloud.qdrant.io", 
+                      api_key=api_key)
+
