57f17b7c7add552630999c8f6522af71b1d546cf
Kaushik Acharya <kaushik@palebluedot.net>
2026-07-13 17:52:31 -0700

    First working fetch

--- qdrant_basics.py
+++ qdrant_basics.py
@@ -10,3 +10,6 @@ api_key = os.getenv("QDRANT_API_KEY")
 client = QdrantClient(url="https://2ec3c57d-71b6-449e-9d63-6c9404bfade3.us-west-1-0.aws.cloud.qdrant.io", 
                       api_key=api_key)
 
+# Retrieve and display the list of collections
+collections = client.get_collections()
+print("Existing collections:", collections)
