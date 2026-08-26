8df8a6a28075901ae48cb4412bf7242a565a53ba
Kaushik Acharya <kaushik@palebluedot.net>
2026-08-26 16:52:50 -0700

    Initial commit

---
+++ qdrant_hybrid_search_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from qdrant_lib import (
+    authenticate_huggingface,
+    get_or_create_collection,
+    get_qdrant_connection,
+)
+from qdrant_client import models
+from sentence_transformers import SentenceTransformer
+import time
+
+client = get_qdrant_connection()
+
+collection_name = "day3_hybrid_search"
+
+# Create hybrid collection
+client.create_collection(
+    collection_name=collection_name,
+    vectors_config={
+        "dense": models.VectorParams(size=384, distance=models.Distance.COSINE)
+    },
+    sparse_vectors_config={
+        "sparse": models.SparseVectorParams(
+            index=models.SparseIndexParams(on_disk=False)
+        )
+    }
+)
+
